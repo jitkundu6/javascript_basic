404baaf79ba394ae078daf0eb9236d551393f50b
Subhajit Kundu <jitkundu6@gmail.com>
2023-12-08 19:51:23 +0530

    JS Variables

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // This is our first Javascipt code.
 console.log("Hello World!");
 console.log('Hi');
+// To execute the code in node, open terminal and type "node index.js"
+
+
+// var name; // before ES6
+let name = 'SK';    
+console.log(name);
+
+// Variables: 
+// Can't be reserved keyword,
+// Should be meaningful,
+// Can't start with numbers (Ex: 1name),
+// Can't contain hyphen '-' or space ' ',
+// As per JS convention variable name should follow camelCase notation (Ex: firstName),
+// Variables are case sensative (Ex: a, A),
+
+//let firstName = 'Subhajit', lastName = 'Kundu';
+let firstName = 'Subhajit';
+let lastName = 'Kundu';
